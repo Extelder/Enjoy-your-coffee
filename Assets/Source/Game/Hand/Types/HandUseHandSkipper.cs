bf75acc089f94a3311b8899cbc0fe4d420820fcd
igoryava <126491702+igoryava@users.noreply.github.com>
2026-02-14 23:41:09 +0700

    done switch consumable

--- Assets/Source/Game/Hand/Types/HandUseHandSkipper.cs
+++ Assets/Source/Game/Hand/Types/HandUseHandSkipper.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandUseHandSwitcher : HandUseConsumable
+public class HandUseHandSkipper : HandUseConsumable
 {
     public override void Use(Hand hand)
     {
-        GameState.Instance.SwitchHand(GameState.Instance.Id + 1);
+        GameState.Instance.SkipHand = true;
     }
 }
