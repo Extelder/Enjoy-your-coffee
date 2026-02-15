3a2a6f8c05fdb776df58613520151ce8999d0396
Extelder <Nikolaoskas15@gmail.com>
2026-02-16 00:07:45 +0700

    Added undless

--- Assets/Source/Game/Health/EnemyHealth.cs
+++ Assets/Source/Game/Health/EnemyHealth.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyHealth : Health
 {
     [SerializeField] private GameObject _prize;
     [SerializeField] private GameObject _prizeOnTable;
+
+    public bool RestartPermanently;
 
     private void OnEnable()
     {
@@ -23,6 +26,11 @@ public class EnemyHealth : Health
 
     public void DeathAnimEnd()
     {
+        if (RestartPermanently)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
         _prize.SetActive(true);
         PlayerPrefs.SetInt("Prize", 1);
     }
